7955f717a5832ce2a4d7a1ab17c84e88dbc9f7b2
Peter Manchev <p.manchev@emerchantpay.com>
2015-01-08 17:15:11 +0200

    Initial commit

---
+++ includes/languages/english/modules/payment/ecomprocessing_genesis.php
@@ -0,0 +1,13 @@
+<?php
+
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_TEXT_TITLE', 'E-ComProcessing Genesis');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_TEXT_PUBLIC_TITLE', 'Credit Card');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="https://e-comprocessing.net" target="_blank" style="text-decoration: underline; font-weight: bold;">Visit E-ComProcessing\'s Website</a>');
+// Form
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_CREDIT_CARD_OWNER', 'Credit Card Holder/Owner');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_CREDIT_CARD_NUMBER', 'Credit Card Number');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_CREDIT_CARD_EXPIRES', 'Credit Card Expiration Date');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_CREDIT_CARD_CVV', 'Credit Card CVV/CVV2/CSC Number');
+// Error
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_ERROR_TITLE', 'Order processing error!');
+define('MODULE_PAYMENT_ECOMPROCESSING_GENESIS_ERROR_DESC', 'An error was reported while processing your order, please try again!');
