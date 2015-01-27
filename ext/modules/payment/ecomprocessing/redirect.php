cff177f120ce796d0585357396cfe35a3fad7c92
Peter Manchev <p.manchev@emerchantpay.com>
2015-01-27 16:08:05 +0200

    Sync to the current eMP version

--- ext/modules/payment/ecomprocessing/redirect.php
+++ ext/modules/payment/ecomprocessing/redirect.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * E-ComProcessing Redirect Handler
+ *
+ * Handler customer redirection in Asynchronous transactions
+ *
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html
+ * @copyright   2015 E-ComProcessing™
+ * @version     $Id:$
+ * @since       1.0.0
+ */
+
+// "Shhh. Be vewy vewy quiet, I'm hunting wabbits"
+ini_set('display_errors', 'Off');
+error_reporting(0);
 
 chdir('../../../../');
 
@@ -15,8 +29,8 @@ $return = isset($_GET['return']) ? strval($_GET['return']) : null;
 switch ($return) {
 	default:
 		break;
+	// Finish what checkout_process started!
 	case 'success':
-		// Finish what checkout_process started!
 		global $cart;
 
 		$cart->reset(true);
@@ -30,10 +44,28 @@ switch ($return) {
 		tep_redirect(tep_href_link(FILENAME_CHECKOUT_SUCCESS, '', 'SSL'));
 		break;
 	case 'failure':
+		global $payment;
+
+		require(DIR_WS_CLASSES . 'payment.php');
+
+		$payment_class = new payment($payment);
+
+		switch($payment) {
+			case 'ecomprocessing_checkout':
+				$error = MODULE_PAYMENT_EMERCHANTPAY_CHECKOUT_ERROR_DESC;
+				break;
+			case 'ecomprocessing_standard':
+				$error = MODULE_PAYMENT_EMERCHANTPAY_STANDARD_ERROR_DESC;
+				break;
+			default:
+				$error = 'Unable to process the transaction, please try again!';
+				break;
+		}
+
 		tep_redirect(
 			tep_href_link(
 				FILENAME_CHECKOUT_PAYMENT,
-				'error_message=' . urlencode(MODULE_PAYMENT_ECOMPROCESSING_GENESIS_ERROR_DESC),
+				'error_message=' . urlencode($error),
 				'SSL'
 			)
 		);
---
+++ includes/languages/english/modules/payment/ecomprocessing_checkout.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * E-ComProcessing Checkout English Language file
+ *
+ * Contains English translation for strings used in the
+ * E-ComProcessing Checkout module
+ *
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html
+ * @copyright   2015 E-ComProcessing™
+ * @version     $Id:$
+ * @since       1.0.0
+ */
+
+define('MODULE_PAYMENT_ECOMPROCESSING_CHECKOUT_TEXT_TITLE', 'E-ComProcessing Checkout');
+define('MODULE_PAYMENT_ECOMPROCESSING_CHECKOUT_TEXT_PUBLIC_TITLE', 'Credit Card (E-ComProcessing Checkout)');
+define('MODULE_PAYMENT_ECOMPROCESSING_CHECKOUT_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://e-comprocessing.com/" target="_blank" style="text-decoration:underline;font-weight:bold;">Visit E-ComProcessing\'s Website</a>');
+// Error
+define('MODULE_PAYMENT_ECOMPROCESSING_CHECKOUT_ERROR_TITLE', 'Order processing error!');
+define('MODULE_PAYMENT_ECOMPROCESSING_CHECKOUT_ERROR_DESC', 'An error was reported while processing your order, please try again!');
