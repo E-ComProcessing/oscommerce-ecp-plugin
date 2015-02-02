66588e0cec6fe0a1a64062609bde1a573c2427d3
Peter Manchev <p.manchev@emerchantpay.com>
2015-02-02 17:11:05 +0200

    Initial commit

---
+++ includes/languages/english/modules/payment/ecomprocessing_standard.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * E-ComProcessing Standard English Language file
+ *
+ * Contains English translation for strings used in the
+ * E-ComProcessing Standard module
+ *
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html
+ * @copyright   2015 E-ComProcessing™
+ * @version     $Id:$
+ * @since       1.0.0
+ */
+
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_TEXT_TITLE', 'E-ComProcessing Standard');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_TEXT_PUBLIC_TITLE', 'Credit Card');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_TEXT_DESCRIPTION', '<img src="images/icon_popup.gif" border="0">&nbsp;<a href="http://e-comprocessing.com/" target="_blank" style="text-decoration:underline;font-weight:bold;">Visit E-ComProcessing\'s Website</a>');
+// Form
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_CREDIT_CARD_OWNER', 'Credit Card Holder');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_CREDIT_CARD_NUMBER', 'Credit Card Number');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_CREDIT_CARD_EXPIRES', 'Credit Card Expiration Date');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_CREDIT_CARD_CVV', 'Credit Card CVV/CVV2/CSC Number');
+// Error
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_ERROR_TITLE', 'Order processing error!');
+define('MODULE_PAYMENT_ECOMPROCESSING_STANDARD_ERROR_DESC', 'An error was reported while processing your order, please try again!');
